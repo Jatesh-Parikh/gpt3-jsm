d5f03ab8e6f0d40ea8f8114d8768dfc507df8588
Jatesh Parikh <jateshparikh97@gmail.com>
2024-06-15 00:02:45 +0530

    features section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Brand from "./components/Brand/Brand";
 import Navbar from "./components/Navbar/Navbar";
+import Features from "./containers/Features/Features";
 import Header from "./containers/Header/Header";
 import WhatGPT3 from "./containers/WhatGPT3/WhatGPT3";
 
@@ -13,7 +14,7 @@ function App() {
       </div>
       <Brand />
       <WhatGPT3 />
-      {/* <Features /> */}
+      <Features />
       {/* <Possibility /> */}
       {/* <CTA /> */}
       {/* <Blog /> */}

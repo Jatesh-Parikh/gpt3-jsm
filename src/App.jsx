86e45662b784b11ae6924f10a0890dc808053c9d
Jatesh Parikh <jateshparikh97@gmail.com>
2024-06-14 21:35:32 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        {/* <Header />  */}
+      </div>
+      {/* <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
